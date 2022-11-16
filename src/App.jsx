92a17483e17ae38bf048d8e501a3a4a560d5c0e5
joluro86 <joluro86@hotmail.com>
2022-11-15 22:30:45 -0500

    elimiar area

--- src/App.jsx
+++ src/App.jsx
@@ -9,16 +9,21 @@ function App() {
 
   const agregarTarea = e => {
     e.preventDefault()
-    if(!tarea.trim()){
+    if (!tarea.trim()) {
       console.log('elemento vacio')
       return
     }
     console.log(tarea)
     setTareas([
-      ...tareas, 
-      {id: nanoid(), nombreTarea:tarea}
+      ...tareas,
+      { id: nanoid(), nombreTarea: tarea }
     ])
     setTarea('')
+  }
+
+  const eliminarTarea= id => {
+    const arrayFiltrado= tareas.filter(item => item.id !== id)
+    setTareas(arrayFiltrado)
   }
 
 
@@ -34,15 +39,22 @@ function App() {
           <div className="col-8">
             <h4 className="text-center">Lista de Tareas</h4>
             <ul className="list-group">
-              <li className="list-group-item" >
-                <span className="lead">Tarea #1</span>
-                <button
-                  className="btn btn-sm btn-danger float-right mx-2"
-                >Eliminar</button>
-                <button
-                  className="btn btn-sm btn-warning float-right"
-                >Editar</button>
-              </li>
+              {
+                tareas.map(item => (
+                  <li className="list-group-item" key={item.id}>
+                    <span className="lead">{item.nombreTarea}</span>
+                    <button
+                      className="btn btn-sm btn-danger float-right mx-2"
+                      onClick={()=> eliminarTarea(item.id)}
+                    >
+                      Eliminar
+                    </button>
+                    <button
+                      className="btn btn-sm btn-warning float-right"
+                    >Editar</button>
+                  </li>
+                ))
+              }
             </ul>
           </div>
 
@@ -55,7 +67,7 @@ function App() {
                 type="text"
                 className="form-control mb-2"
                 placeholder="Ingrese Tarea"
-                onChange={ e => setTarea(e.target.value) }
+                onChange={e => setTarea(e.target.value)}
                 value={tarea}
               />
               <button className="btn btn-dark btn-block" type="submit">Agregar</button>
